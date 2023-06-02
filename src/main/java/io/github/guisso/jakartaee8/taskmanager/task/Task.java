9342e77ee7c9de739b57fd0ccdc082069baeabee
Guisso <luis.guisso@ifnmg.edu.br>
2023-06-02 14:52:11 -0300

    Conclusion date as present or future

--- src/main/java/io/github/guisso/jakartaee8/taskmanager/task/Task.java
+++ src/main/java/io/github/guisso/jakartaee8/taskmanager/task/Task.java
@@ -23,7 +23,7 @@ import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Past;
+import javax.validation.constraints.PastOrPresent;
 
 /**
  *
@@ -99,8 +99,8 @@ public class Task
     private LocalDate expiration;
 
     @Column(columnDefinition = "DATE")
-//    @PastOrPresent // Jakarta EE Validation
-    @Past
+    @PastOrPresent // Jakarta EE Validation
+//    @Past
     private LocalDate conclusion;
 
     @Column(nullable = true)
